d2193e17cf3354445d74cf1a2d22ab0a0dc3aa74
SomMariya <m.som-2030@kpi.ua>
2021-12-30 17:30:40 +0200

    added db

--- src/api/services/service.js
+++ src/api/services/service.js
@@ -1,1 +1,23 @@
-import axios from 'axios';import { HttpError } from '../../http-error';import { Url } from '../../apiConstants';import { envConfig } from '../../../env-config';import { CityData } from '../../models';export const getWeather = async (city_name) => {  try {    const response = await axios.get(      `${Url}/weather?q=${city_name}&appid=${envConfig.apiKey}`    );    console.log(response.data);    await CityData.create(response.data);    return response.data;  } catch {    throw new HttpError({      message: 'Oops! Can not get weather for this city',      status: 404    });  }};
+import axios from 'axios';
+import { HttpError } from '../../http-error';
+import { Url } from '../../apiConstants';
+import { envConfig } from '../../../env-config';
+
+import { CityData } from '../../models';
+
+export const getWeather = async (city_name) => {
+  try {
+    const response = await axios.get(
+      `${Url}/weather?q=${city_name}&appid=${envConfig.apiKey}`
+    );
+    //const city = CityData.find({ name: city_name });
+
+    await CityData.create(response.data);
+    return response.data;
+  } catch {
+    throw new HttpError({
+      message: 'Oops! Can not get weather for this city',
+      status: 404
+    });
+  }
+};
--- src/models/city.model.js
+++ src/models/city.model.js
@@ -1,1 +1,43 @@
-import mongoose, { Schema } from 'mongoose';const citySchema = new Schema(  {    coord: {      lat: Number,      lng: Number    },    weather: [      {        id: Number,        main: String,        description: String,        icon: String      }    ],    base: String,    main: {      temp: Number,      feels_like: Number,      temp_min: Number,      temp_max: Number,      pressure: Number,      humidity: Number    },    visibility: Number,    wind: {      speed: Number,      deg: Number,      gust: Number    },    clouds: {      all: Number    },    dt: Number,    sys: {      type: Number,      id: Number,      country: String,      sunrise: Number,      sunset: Number    },    timezone: Number,    id: Number,    name: String,    cod: Number  },  { versionKey: false });export const CityData = mongoose.model('City', citySchema, 'city');
+import mongoose, { Schema } from 'mongoose';
+
+const citySchema = new Schema(
+  {
+    created_at: { type: Date, expires: '2h', default: Date.now },
+    coord: { lat: Number, lng: Number },
+    weather: [
+      {
+        id: Number,
+        main: String,
+        description: String,
+        icon: String
+      }
+    ],
+    base: String,
+    main: {
+      temp: Number,
+      feels_like: Number,
+      temp_min: Number,
+      temp_max: Number,
+      pressure: Number,
+      humidity: Number
+    },
+    visibility: Number,
+    wind: {
+      speed: Number,
+      deg: Number,
+      gust: Number
+    },
+    clouds: {
+      all: Number
+    },
+    dt: Number,
+    timezone: Number,
+    id: Number,
+    name: String,
+    cod: Number
+  },
+
+  { versionKey: false }
+);
+
+export const CityData = mongoose.model('CityData', citySchema, 'cityData');
